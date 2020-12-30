49d27b8110863bd029e061f93343e483c457a26e
Saad Malik <saad@spectrocloud.com>
2020-12-30 12:02:49 -0800

    Azure docs (#7)
    * add basic examples
    
    * Adding azure docs

---
+++ examples/resources/spectrocloud_cloudaccount_azure/resource.tf
@@ -0,0 +1,6 @@
+resource "spectrocloud_cloudaccount_azure" "azure-1" {
+  name                = "azure-1"
+  azure_tenant_id     = "<....>"
+  azure_client_id     = "<....>"
+  azure_client_secret = "<....>"
+}
