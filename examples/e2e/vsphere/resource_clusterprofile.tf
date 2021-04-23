24b1df4f16617af93c8da4f3fa973e4f34da97bb
Saad Malik <saad@spectrocloud.com>
2021-04-23 15:54:11 -0700

    add fun

--- examples/e2e/vsphere/resource_clusterprofile.tf
+++ examples/e2e/vsphere/resource_clusterprofile.tf
@@ -4,6 +4,87 @@
 #   name = var.cluster_cluster_profile_name
 # }
 
+data "spectrocloud_pack" "byom" {
+  name = "spectro-byo-manifest"
+  # version  = "1.0.x"
+}
+
+data "spectrocloud_pack" "csi" {
+  name = "csi-vsphere-volume"
+  # version  = "1.0.x"
+}
+
+data "spectrocloud_pack" "cni" {
+  name    = "cni-calico"
+  version = "3.16.0"
+}
+
+data "spectrocloud_pack" "k8s" {
+  name    = "kubernetes"
+  version = "1.18.15"
+}
+
+data "spectrocloud_pack" "ubuntu" {
+  name = "ubuntu-vsphere"
+  # version  = "1.0.x"
+}
+
+resource "spectrocloud_cluster_profile" "profile" {
+  name        = "vsphere-picard-2"
+  description = "basic cp"
+  cloud       = "vsphere"
+  type        = "cluster"
+
+  pack {
+    name   = "ubuntu-vsphere"
+    tag    = "LTS__18.4.x"
+    uid    = data.spectrocloud_pack.ubuntu.id
+    values = data.spectrocloud_pack.ubuntu.values
+  }
+
+  pack {
+    name   = "kubernetes"
+    tag    = "1.18.15"
+    uid    = data.spectrocloud_pack.k8s.id
+    values = data.spectrocloud_pack.k8s.values
+  }
+
+  pack {
+    name   = "cni-calico"
+    tag    = "3.16.x"
+    uid    = data.spectrocloud_pack.cni.id
+    values = data.spectrocloud_pack.cni.values
+  }
+
+  pack {
+    name   = "csi-vsphere-volume"
+    tag    = "1.0.x"
+    uid    = data.spectrocloud_pack.csi.id
+    values = data.spectrocloud_pack.csi.values
+  }
+
+  pack {
+    name   = "spectro-byo-manifest"
+    tag    = "1.0.x"
+    uid    = data.spectrocloud_pack.byom.id
+    values = <<-EOT
+      manifests:
+        byo-manifest:
+          contents: |
+            # Add manifests here
+            apiVersion: v1
+            kind: Namespace
+            metadata:
+              labels:
+                app: wordpress
+                app3: wordpress3
+              name: wordpress
+    EOT
+  }
+
+
+}
+
 # # Example of a Basic add-on profile
 # resource "spectrocloud_cluster_profile" "cp-addon-vsphere" {
 #   name        = "cp-basic"
@@ -31,82 +112,3 @@
 #   }
 # }
 
-
-data "spectrocloud_pack" "byom" {
-  name = "spectro-byo-manifest"
-  # version  = "1.0.x"
-}
-
-data "spectrocloud_pack" "csi" {
-  name = "csi-vsphere-volume"
-  # version  = "1.0.x"
-}
-
-data "spectrocloud_pack" "cni" {
-  name    = "cni-calico"
-  version = "3.16.0"
-}
-
-data "spectrocloud_pack" "k8s" {
-  name    = "kubernetes"
-  version = "1.18.14"
-}
-
-data "spectrocloud_pack" "ubuntu" {
-  name = "ubuntu-vsphere"
-  # version  = "1.0.x"
-}
-
-resource "spectrocloud_cluster_profile" "profile" {
-  name        = "vsphere-picard-2"
-  description = "basic cp"
-  cloud       = "vsphere"
-  type        = "cluster"
-
-  pack {
-    name   = "spectro-byo-manifest"
-    tag    = "1.0.x"
-    uid    = data.spectrocloud_pack.byom.id
-    values = <<-EOT
-      manifests:
-        byo-manifest:
-          contents: |
-            # Add manifests here
-            apiVersion: v1
-            kind: Namespace
-            metadata:
-              labels:
-                app: wordpress
-                app3: wordpress3
-              name: wordpress
-    EOT
-  }
-
-  pack {
-    name   = "csi-vsphere-volume"
-    tag    = "1.0.x"
-    uid    = data.spectrocloud_pack.csi.id
-    values = data.spectrocloud_pack.csi.values
-  }
-
-  pack {
-    name   = "cni-calico"
-    tag    = "3.16.x"
-    uid    = data.spectrocloud_pack.cni.id
-    values = data.spectrocloud_pack.cni.values
-  }
-
-  pack {
-    name   = "kubernetes"
-    tag    = "1.18.x"
-    uid    = data.spectrocloud_pack.k8s.id
-    values = data.spectrocloud_pack.k8s.values
-  }
-
-  pack {
-    name   = "ubuntu-vsphere"
-    tag    = "LTS__18.4.x"
-    uid    = data.spectrocloud_pack.ubuntu.id
-    values = data.spectrocloud_pack.ubuntu.values
-  }
-}
