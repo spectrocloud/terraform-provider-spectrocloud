67f2e205094451ee9db9f77c2095a3ddc8cb8978
Rishi <rishi.anand0@gmail.com>
2021-05-07 22:14:43 +0530

    validation func change

--- examples/e2e/eks/resource_cloudaccount.tf
+++ examples/e2e/eks/resource_cloudaccount.tf
@@ -5,7 +5,10 @@
 //}
 
 resource "spectrocloud_cloudaccount_aws" "account" {
-  name           = "aws-dev-access"
-  aws_access_key = var.aws_access_key
-  aws_secret_key = var.aws_secret_key
+  name            = "aws-dev-access"
+  aws_access_key  = var.aws_access_key
+  aws_secret_key  = var.aws_secret_key
+  type            = var.cloud_account_type
+  arn             = var.arn
+  external_id     = var.external_id
 }
