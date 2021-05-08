efdf8b4cc974628d6921fdfffcdea83f7ae5273c
Rishi Anand <rishi@spectrocloud.com>
2021-05-09 00:01:49 +0530

    Adding eks and import examples (#36)

--- examples/resources/spectrocloud_cluster_import/resource.tf
+++ examples/resources/spectrocloud_cluster_import/resource.tf
@@ -1,17 +1,3 @@
----
-page_title: "spectrocloud_cluster_import Resource - terraform-provider-spectrocloud"
-subcategory: ""
-description: |-
-  
----
-
-# Resource `spectrocloud_cluster_import`
-
-
-
-## Example Usage
-
-```terraform
 data "spectrocloud_cluster_profile" "profile" {
   # id = <uid>
   name = var.cluster_cluster_profile_name
@@ -46,45 +32,3 @@ resource "spectrocloud_cluster_import" "cluster" {
        EOT
     }*/
 }
-```
-
-## Schema
-
-### Required
-
-- **cloud** (String)
-- **name** (String)
-
-### Optional
-
-- **cluster_profile_id** (String)
-- **id** (String) The ID of this resource.
-- **pack** (Block List) (see [below for nested schema](#nestedblock--pack))
-- **timeouts** (Block, Optional) (see [below for nested schema](#nestedblock--timeouts))
-
-### Read-only
-
-- **cloud_config_id** (String)
-- **cluster_import_manifest** (String)
-- **cluster_import_manifest_apply_command** (String)
-
-<a id="nestedblock--pack"></a>
-### Nested Schema for `pack`
-
-Required:
-
-- **name** (String)
-- **tag** (String)
-- **values** (String)
-
-
-<a id="nestedblock--timeouts"></a>
-### Nested Schema for `timeouts`
-
-Optional:
-
-- **create** (String)
-- **delete** (String)
-- **update** (String)
-
-
---
+++ examples/resources/spectrocloud_cluster_import/variables.tf
@@ -0,0 +1,10 @@
+variable "sc_host" {}
+variable "sc_username" {}
+variable "sc_password" {}
+variable "sc_project_name" {}
+
+variable "cluster_cloud_account_name" {}
+variable "cluster_cluster_profile_name" {}
+
+variable "cluster_name" {}
+variable "cloud_type" {}
