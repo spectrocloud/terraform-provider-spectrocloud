44e92c0b24e38408d92b5411cb0802d5bdaedcbf
Akhilesh Verma <akhlesh.vermaofc@gmail.com>
2022-03-03 02:28:53 +0530

    BET-3646: Terraform - Failed to create vsphere cloud account (#68)
    change description: Code commented in resource_cloudacount.tf file, until we have support for vsphere account

--- examples/e2e/vsphere/resource_cloudaccount.tf
+++ examples/e2e/vsphere/resource_cloudaccount.tf
@@ -1,14 +1,4 @@
+
 data "spectrocloud_cloudaccount_vsphere" "account" {
   name = var.shared_vmware_cloud_account_name
 }
-
-# If creating a new cloud account, use this:
-#
-# resource "spectrocloud_cloudaccount_vsphere" "account" {
-#   name                 = "vsphere-picard-2"
-#   private_cloud_gateway_id      = var.private_cloud_gateway_id
-#   vsphere_vcenter               = "<....>"
-#   vsphere_username              = "<....>"
-#   vsphere_password              = "<....>"
-#   vsphere_ignore_insecure_error = true
-# }
