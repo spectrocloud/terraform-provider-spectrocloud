e1c7cb56efb2b8cd7cbef7ce3b927cfecd0a4b30
Nikolay <nikchern@gmail.com>
2022-08-01 14:56:34 -0700

    BET-5998: support manifest for maas, openstack, aws, azure, aks, gcp.

--- spectrocloud/resource_cluster_openstack.go
+++ spectrocloud/resource_cluster_openstack.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"sort"
+	"strings"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -55,6 +56,11 @@ func resourceClusterOpenStack() *schema.Resource {
 							Optional: true,
 							Elem: &schema.Resource{
 								Schema: map[string]*schema.Schema{
+									"type": {
+										Type:     schema.TypeString,
+										Optional: true,
+										Default:  "spectro",
+									},
 									"name": {
 										Type:     schema.TypeString,
 										Required: true,
@@ -65,11 +71,34 @@ func resourceClusterOpenStack() *schema.Resource {
 									},
 									"tag": {
 										Type:     schema.TypeString,
-										Required: true,
+										Optional: true,
 									},
 									"values": {
 										Type:     schema.TypeString,
 										Required: true,
+									},
+									"manifest": {
+										Type:     schema.TypeList,
+										Optional: true,
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"name": {
+													Type:     schema.TypeString,
+													Required: true,
+												},
+												"content": {
+													Type:     schema.TypeString,
+													Required: true,
+													DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
+														// UI strips the trailing newline on save
+														if strings.TrimSpace(old) == strings.TrimSpace(new) {
+															return true
+														}
+														return false
+													},
+												},
+											},
+										},
 									},
 								},
 							},
