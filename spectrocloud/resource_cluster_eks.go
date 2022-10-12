a872f77b50c864b75670073f656743c32c76779b
Nikolay <nikchern@gmail.com>
2022-10-12 14:30:12 -0700

    PLT-87: Pass host configuration to EKS cluster on create.

--- spectrocloud/resource_cluster_eks.go
+++ spectrocloud/resource_cluster_eks.go
@@ -945,10 +945,7 @@ func toEksCluster(c *client.V1Client, d *schema.ResourceData) *models.V1SpectroE
 	}
 
 	cluster.Spec.Machinepoolconfig = machinePoolConfigs
-	cluster.Spec.ClusterConfig = &models.V1ClusterConfigEntity{
-		Resources:               toClusterResourceConfig(d),
-		MachineManagementConfig: toMachineManagementConfig(d),
-	}
+	cluster.Spec.ClusterConfig = toClusterConfig(d)
 
 	fargateProfiles := make([]*models.V1FargateProfile, 0)
 	for _, fargateProfile := range d.Get("fargate_profile").([]interface{}) {
