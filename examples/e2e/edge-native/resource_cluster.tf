be45881f0c25a0c0b427b0fb6a7acbb474c1c364
Nikolay <nikchern@gmail.com>
2022-10-14 09:30:54 -0700

    PLT-90: configuration cleanup.

--- examples/e2e/edge-native/resource_cluster.tf
+++ examples/e2e/edge-native/resource_cluster.tf
@@ -15,32 +15,15 @@ resource "spectrocloud_cluster_edge_native" "cluster" {
     control_plane           = true
     control_plane_as_worker = true
     name                    = "master-pool"
-    count                   = 1
 
     host_uids     = [spectrocloud_appliance.appliance0.uid]
 
-    /*instance_type {
-      name = "small"
-      disk_size_gb = 30
-      memory_mb    = 8096
-      cpu          = 4
-    }*/
   }
 
   machine_pool {
     name = "worker-pool"
-    min           = 2
-    max = 3
-    count = 3
-
     host_uids     = [spectrocloud_appliance.appliance1.uid]
 
-    /*instance_type {
-      name = "large"
-      disk_size_gb = 30
-      memory_mb    = 8096
-      cpu          = 2
-    }*/
   }
 
 }
