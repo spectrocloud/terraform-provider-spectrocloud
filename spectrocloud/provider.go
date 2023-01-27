b53e802b49e8f90fa6335fa5063979db3cb9727b
nikchern <nikchern@users.noreply.github.com>
2023-01-27 11:55:38 -0800

    Update spectrocloud/provider.go

--- spectrocloud/provider.go
+++ spectrocloud/provider.go
@@ -160,7 +160,7 @@ func New(_ string) func() *schema.Provider {
 				"spectrocloud_cluster_group":       dataSourceClusterGroup(),
 				"spectrocloud_application_profile": dataSourceApplicationProfile(),
 				"spectrocloud_workspace":           dataSourceWorkspace(),
-				"spectrocloud_private_cloud_gateway":
+				"spectrocloud_private_cloud_gateway": dataSourcePCG(),
 			},
 			ConfigureContextFunc: providerConfigure,
 		}
