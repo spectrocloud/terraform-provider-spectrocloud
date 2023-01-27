be11d104fd9f632b9dad9619d8059fea390ce5ff
nikchern <nikchern@users.noreply.github.com>
2023-01-26 21:28:47 -0800

    PLT-257-1: fixing compile errors and adding unit tests. (#206)
    Co-authored-by: nikolay-spectro <nikolay@spectrocloud.com>

---
+++ spectrocloud/resource_cloud_account_vsphere_test.go
@@ -0,0 +1,72 @@
+package spectrocloud
+
+import (
+	"github.com/spectrocloud/hapi/models"
+	"github.com/spectrocloud/terraform-provider-spectrocloud/types"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestToVsphereAccount(t *testing.T) {
+	rd := resourceCloudAccountVsphere().TestResourceData()
+	rd.Set("name", "vsphere_unit_test_acc")
+	rd.Set("vsphere_vcenter", "vcenter.example.com")
+	rd.Set("vsphere_username", "testuser")
+	rd.Set("vsphere_password", "testpass")
+	rd.Set("vsphere_ignore_insecure_error", false)
+	rd.Set("private_cloud_gateway_id", "12345")
+	acc := toVsphereAccount(rd)
+
+	assert.Equal(t, rd.Get("name"), acc.Metadata.Name)
+	assert.Equal(t, rd.Get("vsphere_vcenter"), *acc.Spec.VcenterServer)
+	assert.Equal(t, rd.Get("vsphere_username"), *acc.Spec.Username)
+	assert.Equal(t, rd.Get("vsphere_password"), *acc.Spec.Password)
+	assert.Equal(t, rd.Get("vsphere_ignore_insecure_error"), acc.Spec.Insecure)
+	assert.Equal(t, rd.Get("private_cloud_gateway_id"), acc.Metadata.Annotations[OverlordUID])
+	assert.Equal(t, rd.Id(), acc.Metadata.UID)
+}
+
+func TestToVsphereAccountIgnoreInsecureError(t *testing.T) {
+	rd := resourceCloudAccountVsphere().TestResourceData()
+	rd.Set("name", "vsphere_unit_test_acc")
+	rd.Set("vsphere_vcenter", "vcenter.example.com")
+	rd.Set("vsphere_username", "testuser")
+	rd.Set("vsphere_password", "testpass")
+	rd.Set("vsphere_ignore_insecure_error", true)
+	rd.Set("private_cloud_gateway_id", "67890")
+	acc := toVsphereAccount(rd)
+
+	assert.Equal(t, rd.Get("name"), acc.Metadata.Name)
+	assert.Equal(t, rd.Get("vsphere_vcenter"), *acc.Spec.VcenterServer)
+	assert.Equal(t, rd.Get("vsphere_username"), *acc.Spec.Username)
+	assert.Equal(t, rd.Get("vsphere_password"), *acc.Spec.Password)
+	assert.Equal(t, rd.Get("vsphere_ignore_insecure_error"), acc.Spec.Insecure)
+	assert.Equal(t, rd.Get("private_cloud_gateway_id"), acc.Metadata.Annotations[OverlordUID])
+	assert.Equal(t, rd.Id(), acc.Metadata.UID)
+}
+
+func TestFlattenVsphereCloudAccount(t *testing.T) {
+	rd := resourceCloudAccountVsphere().TestResourceData()
+	account := &models.V1VsphereAccount{
+		Metadata: &models.V1ObjectMeta{
+			Name:        "test_account",
+			Annotations: map[string]string{OverlordUID: "12345"},
+			UID:         "abcdef",
+		},
+		Spec: &models.V1VsphereCloudAccount{
+			VcenterServer: types.Ptr("vcenter.example.com"),
+			Username:      types.Ptr("testuser"),
+			Insecure:      true,
+		},
+	}
+
+	diags, hasError := flattenVsphereCloudAccount(rd, account)
+
+	assert.Nil(t, diags)
+	assert.False(t, hasError)
+	assert.Equal(t, "test_account", rd.Get("name"))
+	assert.Equal(t, "12345", rd.Get("private_cloud_gateway_id"))
+	assert.Equal(t, "vcenter.example.com", rd.Get("vsphere_vcenter"))
+	assert.Equal(t, "testuser", rd.Get("vsphere_username"))
+	assert.Equal(t, true, rd.Get("vsphere_ignore_insecure_error"))
+}
