42c4af80fec44ba2b8bfa3a0b3f04a3d2ea35451
nikolay-spectro <nikolay@spectrocloud.com>
2023-03-17 11:36:36 -0700

    vmm-provider-version: regenerating docs.

--- docs/data-sources/cluster_group.md
+++ docs/data-sources/cluster_group.md
@@ -23,12 +23,21 @@ output "out_beehive" {
 }
 
 data "spectrocloud_cluster_group" "tenant_cl" {
-  name    = "tenant_cl"
+  name    = "sanfrancisco"
   context = "tenant"
 }
 
 output "out_tenant_cl" {
   value = data.spectrocloud_cluster_group.tenant_cl.id
+}
+
+data "spectrocloud_cluster_group" "project_sc" {
+  name    = "cg-1"
+  context = "project"
+}
+
+output "out_project_sc" {
+  value = data.spectrocloud_cluster_group.project_sc.id
 }
 ```
 
--- examples/e2e/vsphere_virtual_machine/resource_vm.tf
+++ examples/e2e/vsphere_virtual_machine/resource_vm.tf
@@ -1,14 +1,14 @@
 resource "virtual_machine" "example" {
-  cluster_uid        = spectrocloud_cluster_vsphere.cluster.id
-  name              = var.name
-  namespace         = var.namespace
+  cluster_uid = spectrocloud_cluster_vsphere.cluster.id
+  name        = var.name
+  namespace   = var.namespace
 
   /*template {
     id = var.template_uid
   }*/
-  state           = var.state
-  cpu_cores         = var.cpu_cores
-  memory            = var.memory
-  image             = var.image
+  state               = var.state
+  cpu_cores           = var.cpu_cores
+  memory              = var.memory
+  image               = var.image
   cloudinit_user_data = var.cloudinit_user_data
 }
