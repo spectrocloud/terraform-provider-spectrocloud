81125cc93d3d9bef569010c284bfc3a95813fede
nikchern <nikchern@users.noreply.github.com>
2023-04-22 20:07:42 -0700

    PLT-413: Refresh GCP example. (#256)
    Co-authored-by: nikolay-spectro <nikolay@spectrocloud.com>

--- examples/e2e/gcp/resource_cluster.tf
+++ examples/e2e/gcp/resource_cluster.tf
@@ -1,49 +1,36 @@
+data "spectrocloud_cloudaccount_gcp" "account" {
+  name = var.gcp-cloud-account-name
+}
+
 resource "spectrocloud_cluster_gcp" "cluster" {
-  name               = "gcp-picard-2"
-  cluster_profile_id = spectrocloud_cluster_profile.profile.id
-  cloud_account_id   = spectrocloud_cloudaccount_gcp.account.id
+  name             = "gcp-cluster"
+  tags             = ["gcp", "tutorial"]
+  cloud_account_id = data.spectrocloud_cloudaccount_gcp.account.id
 
   cloud_config {
-    network = var.gcp_network
-    project = var.gcp_project
-    region  = var.gcp_region
+    project = var.gcp-cloud-account-name
+    region  = var.region
   }
 
-  # To override or specify values for a cluster:
-
-  # pack {
-  #   name   = "spectro-byo-manifest"
-  #   tag    = "1.0.x"
-  #   values = <<-EOT
-  #     manifests:
-  #       byo-manifest:
-  #         contents: |
-  #           # Add manifests here
-  #           apiVersion: v1
-  #           kind: Namespace
-  #           metadata:
-  #             labels:
-  #               app: wordpress
-  #               app2: wordpress2
-  #             name: wordpress
-  #   EOT
-  # }
+  cluster_profile {
+    id = spectrocloud_cluster_profile.profile.id
+  }
 
   machine_pool {
     control_plane           = true
     control_plane_as_worker = true
     name                    = "master-pool"
-    count                   = 1
-    instance_type           = "e2-standard-2"
-    disk_size_gb            = 62
-    azs                     = ["${var.gcp_region}-a"]
+    count                   = var.master_nodes.count
+    instance_type           = var.master_nodes.instance_type
+    disk_size_gb            = var.master_nodes.disk_size_gb
+    azs                     = var.master_nodes.availability_zones
   }
 
   machine_pool {
     name          = "worker-basic"
-    count         = 1
-    instance_type = "e2-standard-2"
-    azs           = ["${var.gcp_region}-a"]
+    count         = var.worker_nodes.count
+    instance_type = var.worker_nodes.instance_type
+    disk_size_gb  = var.worker_nodes.disk_size_gb
+    azs           = var.worker_nodes.availability_zones
   }
-
 }
