528b042b6e07507e0ee1b90351964b3cf70809d3
Sivaanand Murugesan <sivaanand.m@gmail.com>
2023-05-22 17:41:25 +0530

    PEM-1529:Handled hapi retry latency error for macros

--- pkg/client/macros.go
+++ pkg/client/macros.go
@@ -1,11 +1,14 @@
 package client
 
 import (
-	"hash/fnv"
-	"strconv"
-
+	"errors"
+	"fmt"
 	"github.com/spectrocloud/hapi/models"
 	userC "github.com/spectrocloud/hapi/user/client/v1"
+	"hash/fnv"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func (h *V1Client) CreateMacros(uid string, macros *models.V1Macros) error {
@@ -15,22 +18,68 @@ func (h *V1Client) CreateMacros(uid string, macros *models.V1Macros) error {
 	}
 	if uid != "" {
 		params := userC.NewV1ProjectsUIDMacrosCreateParams().WithContext(h.Ctx).WithUID(uid).WithBody(macros)
-		_, err := client.V1ProjectsUIDMacrosCreate(params)
-		if err != nil {
-			return err
-		}
+		_, err = client.V1ProjectsUIDMacrosCreate(params)
 	} else {
-		tenantUID, err := h.GetTenantUID()
+		var tenantUID string
+		tenantUID, err = h.GetTenantUID()
 		if err != nil {
 			return err
 		}
 		params := userC.NewV1TenantsUIDMacrosCreateParams().WithContext(h.Ctx).WithTenantUID(tenantUID).WithBody(macros)
 		_, err = client.V1TenantsUIDMacrosCreate(params)
+	}
+	if err != nil {
+		err = h.handleMacroDuplicateForbiddenError(macros, uid, err)
 		if err != nil {
 			return err
 		}
 	}
 
+	return nil
+}
+
+func (h *V1Client) handleMacroDuplicateForbiddenError(macros *models.V1Macros, projectUID string, srcErr error) error {
+	if strings.Contains(srcErr.Error(), "Code:DuplicateMacroNamesForbidden") {
+		var srcMacro *models.V1Macro
+		srcMacro = macros.Macros[0]
+		actualMacro, err := h.retryGetMacro(3, 10*time.Second, srcMacro.Name, projectUID)
+		if err != nil {
+			return err
+		}
+		if actualMacro != nil {
+			if (srcMacro.Name == actualMacro.Name) && (srcMacro.Value == actualMacro.Value) {
+				return nil
+			} else if (srcMacro.Name == actualMacro.Name) && (srcMacro.Value != actualMacro.Value) {
+				return srcErr
+			}
+		} else {
+			return errors.New("code: Failed to create macro")
+		}
+	} else {
+		return srcErr
+	}
+	return nil
+}
+func (h *V1Client) retryGetMacro(attempts int, sleep time.Duration, macroName string, projectUid string) (*models.V1Macro, error) {
+	var macro *models.V1Macro
+	for i := 0; i < attempts; i++ {
+		if i > 0 {
+			time.Sleep(sleep)
+		}
+		macro, _ = h.GetMacro(macroName, projectUid)
+		if macro != nil {
+			return macro, nil
+		}
+	}
+	return nil, fmt.Errorf("after %d attempts, last error: %s", attempts, errors.New("macro get error"))
+}
+
+func (h *V1Client) handleMacroNotFoundError(err error) error {
+	if strings.Contains(err.Error(), "Code:MacrosNotFound") || strings.Contains(err.Error(), "Code:ResourceNotFound") {
+		return nil
+	} else {
+		return err
+	}
 	return nil
 }
 
@@ -117,22 +166,22 @@ func (h *V1Client) DeleteMacros(uid string, body *models.V1Macros) error {
 
 	if uid != "" {
 		params := userC.NewV1ProjectsUIDMacrosDeleteByMacroNameParams().WithContext(h.Ctx).WithUID(uid).WithBody(body)
-		_, err := client.V1ProjectsUIDMacrosDeleteByMacroName(params)
+		_, err = client.V1ProjectsUIDMacrosDeleteByMacroName(params)
 		if err != nil {
-			return err
+			err = h.handleMacroNotFoundError(err)
 		}
 	} else {
-		tenantUID, err := h.GetTenantUID()
+		var tenantUID string
+		tenantUID, err = h.GetTenantUID()
 		if err != nil {
 			return err
 		}
 		params := userC.NewV1TenantsUIDMacrosDeleteByMacroNameParams().WithContext(h.Ctx).WithTenantUID(tenantUID).WithBody(body)
 		_, err = client.V1TenantsUIDMacrosDeleteByMacroName(params)
 		if err != nil {
-			return err
+			err = h.handleMacroNotFoundError(err)
 		}
 	}
-	_, err = h.GetMacros(uid)
 	if err != nil {
 		return err
 	}
