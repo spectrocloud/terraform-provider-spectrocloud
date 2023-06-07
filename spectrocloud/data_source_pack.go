5d667ecfe1ece16c62d7f204203b2ec218371ff1
Sivaanand Murugesan <sivaanand.m@gmail.com>
2023-06-07 14:09:11 +0530

    PLT-486:Adding support for oci/ecr registry in cluster profile

--- spectrocloud/data_source_pack.go
+++ spectrocloud/data_source_pack.go
@@ -85,6 +85,26 @@ func dataSourcePackRead(_ context.Context, d *schema.ResourceData, m interface{}
 				}
 			}
 		}
+		if v.(string) == "oci" {
+			if regUID, ok := d.GetOk("registry_uid"); ok {
+				_, err := c.GetOciBasicRegistry(regUID.(string))
+				if err != nil {
+					return diag.FromErr(err)
+				}
+				// we don't have provision to get all helm chart/packs from oci basic type registry, hence skipping validation
+				return diags
+			}
+		}
+		if v.(string) == "ecr" {
+			if regUID, ok := d.GetOk("registry_uid"); ok {
+				_, err := c.GetOciRegistry(regUID.(string))
+				if err != nil {
+					return diag.FromErr(err)
+				}
+				// we don't have provision to get all helm chart/packs from ECR type registry, hence skipping validation
+				return diags
+			}
+		}
 	}
 
 	filters := make([]string, 0)
