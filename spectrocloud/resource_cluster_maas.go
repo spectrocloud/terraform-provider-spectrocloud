ec3a6716a095c48e3182a285af5ca3385bf4f337
nikchern <nikolay@spectrocloud.com>
2023-06-26 22:15:08 -0700

    Docs refresh for 0.14.2. (#305)
    * Docs refresh for 0.14.2.
    
    * Min max description add for 0.14.2.

--- spectrocloud/resource_cluster_maas.go
+++ spectrocloud/resource_cluster_maas.go
@@ -138,12 +138,14 @@ func resourceClusterMaas() *schema.Resource {
 							Description: "Number of nodes in the machine pool.",
 						},
 						"min": {
-							Type:     schema.TypeInt,
-							Optional: true,
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Description: "Minimum number of nodes in the machine pool. This is used for autoscaling the machine pool.",
 						},
 						"max": {
-							Type:     schema.TypeInt,
-							Optional: true,
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Description: "Maximum number of nodes in the machine pool. This is used for autoscaling the machine pool.",
 						},
 						"instance_type": {
 							Type:     schema.TypeList,
