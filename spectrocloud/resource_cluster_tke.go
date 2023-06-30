b0ff395c93ae44be2df0dce23770c91d886c893e
nikchern <nikolay@spectrocloud.com>
2023-06-29 22:20:23 -0700

    Update spectrocloud/resource_cluster_tke.go

--- spectrocloud/resource_cluster_tke.go
+++ spectrocloud/resource_cluster_tke.go
@@ -332,6 +332,7 @@ func resourceClusterTkeUpdate(ctx context.Context, d *schema.ResourceData, m int
 
 		for _, mp := range ns {
 			machinePoolResource := mp.(map[string]interface{})
+                        // since known issue in TF SDK: https://github.com/hashicorp/terraform-plugin-sdk/issues/588
 			if machinePoolResource["name"].(string) != "" {
 				name := machinePoolResource["name"].(string)
 				hash := resourceMachinePoolTkeHash(machinePoolResource)
