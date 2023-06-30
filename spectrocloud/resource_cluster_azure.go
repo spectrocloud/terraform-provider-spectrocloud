3a307e9c48b52167fef9997f4919992e59800e7c
nikchern <nikolay@spectrocloud.com>
2023-06-29 22:19:07 -0700

    Update spectrocloud/resource_cluster_azure.go

--- spectrocloud/resource_cluster_azure.go
+++ spectrocloud/resource_cluster_azure.go
@@ -383,6 +383,7 @@ func resourceClusterAzureUpdate(ctx context.Context, d *schema.ResourceData, m i
 
 		for _, mp := range ns.List() {
 			machinePoolResource := mp.(map[string]interface{})
+                        // since known issue in TF SDK: https://github.com/hashicorp/terraform-plugin-sdk/issues/588
 			if machinePoolResource["name"].(string) != "" {
 				name := machinePoolResource["name"].(string)
 				hash := resourceMachinePoolAzureHash(machinePoolResource)
