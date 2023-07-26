a0faf1be49c11040d45840ae8202103908178ab8
Sivaanand Murugesan <sivaanand.m@gmail.com>
2023-07-26 09:56:24 +0530

    Update spectrocloud/resource_cluster_aws.go
    Co-authored-by: nikchern <nikchern@gmail.com>

--- spectrocloud/resource_cluster_aws.go
+++ spectrocloud/resource_cluster_aws.go
@@ -171,7 +171,7 @@ func resourceClusterAws() *schema.Resource {
 							Type:        schema.TypeInt,
 							Optional:    true,
 							Default:     0,
-							Description: "Minimum number of seconds a node should be Ready, before the next node is selected for repave (Applicable only for worker pools)",
+							Description: "Minimum number of seconds node should be Ready, before the next node is selected for repave. Default value is `0`",
 						},
 						"capacity_type": {
 							Type:         schema.TypeString,
