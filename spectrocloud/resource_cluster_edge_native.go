68cc245cc566ebf0d67212b4f590f052b4b1c136
Sivaanand Murugesan <sivaanand.m@gmail.com>
2023-07-26 09:58:13 +0530

    Update spectrocloud/resource_cluster_edge_native.go
    Co-authored-by: nikchern <nikchern@gmail.com>

--- spectrocloud/resource_cluster_edge_native.go
+++ spectrocloud/resource_cluster_edge_native.go
@@ -175,7 +175,7 @@ func resourceClusterEdgeNative() *schema.Resource {
 							Type:        schema.TypeInt,
 							Optional:    true,
 							Default:     0,
-							Description: "Minimum number of seconds a node should be Ready, before the next node is selected for repave (Applicable only for worker pools)",
+							Description: "Minimum number of seconds node should be Ready, before the next node is selected for repave. Default value is `0`",
 						},
 						"host_uids": {
 							Type:       schema.TypeList,
