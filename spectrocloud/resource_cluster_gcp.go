3942d3784f570bf3b9f729c8fbf71874265424d0
Sivaanand Murugesan <sivaanand.m@gmail.com>
2023-08-23 17:29:48 +0530

    Added node maintenance support for Gcp

--- spectrocloud/resource_cluster_gcp.go
+++ spectrocloud/resource_cluster_gcp.go
@@ -126,6 +126,7 @@ func resourceClusterGcp() *schema.Resource {
 								Type: schema.TypeString,
 							},
 						},
+						"node":   schemas.NodeSchema(),
 						"taints": schemas.ClusterTaintsSchema(),
 						"control_plane": {
 							Type:     schema.TypeBool,
@@ -260,6 +261,10 @@ func flattenCloudConfigGcp(configUID string, d *schema.ResourceData, c *client.V
 		return diag.FromErr(err)
 	} else {
 		mp := flattenMachinePoolConfigsGcp(config.Spec.MachinePoolConfig)
+		mp, err := flattenNodeMaintenanceStatus(c, c.GetMachinesItemsActionsGcp, mp, configUID, ClusterContext)
+		if err != nil {
+			return diag.FromErr(err)
+		}
 		if err := d.Set("machine_pool", mp); err != nil {
 			return diag.FromErr(err)
 		}
@@ -306,6 +311,10 @@ func resourceClusterGcpUpdate(ctx context.Context, d *schema.ResourceData, m int
 
 	cloudConfigId := d.Get("cloud_config_id").(string)
 	ClusterContext := d.Get("context").(string)
+	CloudConfig, err := c.GetCloudConfigGcp(cloudConfigId, ClusterContext)
+	if err != nil {
+		return diag.FromErr(err)
+	}
 	if d.HasChange("machine_pool") {
 		oraw, nraw := d.GetChange("machine_pool")
 		if oraw == nil {
@@ -323,9 +332,10 @@ func resourceClusterGcpUpdate(ctx context.Context, d *schema.ResourceData, m int
 			machinePool := mp.(map[string]interface{})
 			osMap[machinePool["name"].(string)] = machinePool
 		}
-
+		nsMap := make(map[string]interface{})
 		for _, mp := range ns.List() {
 			machinePoolResource := mp.(map[string]interface{})
+			nsMap[machinePoolResource["name"].(string)] = machinePoolResource
 			// since known issue in TF SDK: https://github.com/hashicorp/terraform-plugin-sdk/issues/588
 			if machinePoolResource["name"].(string) != "" {
 				name := machinePoolResource["name"].(string)
@@ -342,6 +352,11 @@ func resourceClusterGcpUpdate(ctx context.Context, d *schema.ResourceData, m int
 				} else if hash != resourceMachinePoolGcpHash(oldMachinePool) {
 					log.Printf("Change in machine pool %s", name)
 					err = c.UpdateMachinePoolGcp(cloudConfigId, ClusterContext, machinePool)
+					// Node Maintenance Actions
+					err := resourceNodeAction(c, ctx, nsMap[name], c.GetNodeMaintenanceStatusGcp, CloudConfig.Kind, ClusterContext, cloudConfigId, name)
+					if err != nil {
+						return diag.FromErr(err)
+					}
 				}
 
 				if err != nil {
