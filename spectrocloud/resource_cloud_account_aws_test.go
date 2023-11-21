9bea0389e3bd5a55d09adff5c06d254f38529cf6
nikchern <nikchern@gmail.com>
2023-11-20 17:52:38 -0800

    PLT-801: Azure Gov Cloud account support. (#375)
    * PLT-801: Azure Gov Cloud account support.
    
    * PLT-801: Azure Gov Cloud account support doc update.
    
    * PLT-793: Maas cluster import support.
    
    * PLT-793: MAAS cluster documentation update.

--- spectrocloud/resource_cloud_account_aws_test.go
+++ spectrocloud/resource_cloud_account_aws_test.go
@@ -3,7 +3,11 @@ package spectrocloud
 import (
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/spectrocloud/hapi/models"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/spectrocloud/terraform-provider-spectrocloud/types"
 )
 
 func TestToAwsAccountCTXProjectSecret(t *testing.T) {
@@ -30,6 +34,7 @@ func TestToAwsAccountCTXTenantSecret(t *testing.T) {
 	rd.Set("aws_secret_key", "sasf1424aqsfsdf123423SDFs23412sadf@#$@#$")
 	rd.Set("context", "tenant")
 	rd.Set("type", "secret")
+	rd.Set("partition", "test_partition")
 	acc, err := toAwsAccount(rd)
 	assert.NoError(t, err)
 
@@ -38,6 +43,7 @@ func TestToAwsAccountCTXTenantSecret(t *testing.T) {
 	assert.Equal(t, rd.Get("aws_secret_key"), acc.Spec.SecretKey)
 	assert.Equal(t, "tenant", acc.Metadata.Annotations["scope"])
 	assert.Equal(t, rd.Get("type"), string(acc.Spec.CredentialType))
+	assert.Equal(t, rd.Get("partition"), *acc.Spec.Partition)
 }
 
 func TestToAwsAccountCTXProjectSTS(t *testing.T) {
@@ -72,4 +78,95 @@ func TestToAwsAccountCTXTenantSTS(t *testing.T) {
 	assert.Equal(t, rd.Get("external_id"), acc.Spec.Sts.ExternalID)
 	assert.Equal(t, "tenant", acc.Metadata.Annotations["scope"])
 	assert.Equal(t, rd.Get("type"), string(acc.Spec.CredentialType))
+}
+
+func TestFlattenCloudAccountAwsSTS(t *testing.T) {
+	// Create a mock ResourceData object
+	rd := resourceCloudAccountAws().TestResourceData() // Assuming this method exists
+
+	// Create a mock AWS account model
+	account := &models.V1AwsAccount{
+		Metadata: &models.V1ObjectMeta{
+			Name: "aws_test_account",
+			Annotations: map[string]string{
+				"scope": "aws_scope_test",
+			},
+		},
+		Spec: &models.V1AwsCloudAccount{
+			CredentialType: models.V1AwsCloudAccountCredentialTypeSts,
+			Sts:            &models.V1AwsStsCredentials{Arn: "test_arn"},
+			Partition:      types.Ptr("test_partition"),
+			PolicyARNs:     []string{"arn:aws:test_policy1", "arn:aws:test_policy2"},
+		},
+	}
+
+	// Call the flatten function
+	diags, hasError := flattenCloudAccountAws(rd, account)
+
+	// Assertions
+	assert.Nil(t, diags)
+	assert.False(t, hasError)
+	assert.Equal(t, "aws_test_account", rd.Get("name"))
+	assert.Equal(t, "aws_scope_test", rd.Get("context"))
+	assert.Equal(t, "test_arn", rd.Get("arn"))
+	assert.Equal(t, "test_partition", rd.Get("partition"))
+
+	// Handle policy_arns as a *schema.Set
+	policyARNs, ok := rd.Get("policy_arns").(*schema.Set)
+	if !ok {
+		t.Fatalf("Expected policy_arns to be a *schema.Set")
+	}
+
+	var actualARNs []string
+	for _, v := range policyARNs.List() {
+		actualARNs = append(actualARNs, v.(string))
+	}
+
+	expectedARNs := []string{"arn:aws:test_policy1", "arn:aws:test_policy2"}
+	assert.ElementsMatch(t, expectedARNs, actualARNs)
+}
+
+func TestFlattenCloudAccountAws_NonStsType(t *testing.T) {
+	rd := resourceCloudAccountAws().TestResourceData()
+
+	account := &models.V1AwsAccount{
+		Metadata: &models.V1ObjectMeta{
+			Name: "aws_test_account_secret",
+			Annotations: map[string]string{
+				"scope": "aws_scope_test_secret",
+			},
+		},
+		Spec: &models.V1AwsCloudAccount{
+			CredentialType: models.V1AwsCloudAccountCredentialTypeSecret,
+			AccessKey:      "test_access_key_secret",
+			Partition:      types.Ptr("test_partition_secret"),
+			PolicyARNs:     []string{"arn:aws:test_policy_secret1", "arn:aws:test_policy_secret2"},
+		},
+	}
+
+	// Call the flatten function
+	diags, hasError := flattenCloudAccountAws(rd, account)
+
+	// Assertions
+	assert.Nil(t, diags)
+	assert.False(t, hasError)
+	assert.Equal(t, "aws_test_account_secret", rd.Get("name"))
+	assert.Equal(t, "aws_scope_test_secret", rd.Get("context"))
+	assert.Equal(t, "test_access_key_secret", rd.Get("aws_access_key"))
+	assert.Empty(t, rd.Get("arn")) // Asserting that arn is not set
+	assert.Equal(t, "test_partition_secret", rd.Get("partition"))
+
+	// Handle policy_arns as a *schema.Set
+	policyARNs, ok := rd.Get("policy_arns").(*schema.Set)
+	if !ok {
+		t.Fatalf("Expected policy_arns to be a *schema.Set")
+	}
+
+	var actualARNs []string
+	for _, v := range policyARNs.List() {
+		actualARNs = append(actualARNs, v.(string))
+	}
+
+	expectedARNs := []string{"arn:aws:test_policy_secret1", "arn:aws:test_policy_secret2"}
+	assert.ElementsMatch(t, expectedARNs, actualARNs)
 }
--- spectrocloud/resource_cluster_maas.go
+++ spectrocloud/resource_cluster_maas.go
@@ -22,7 +22,10 @@ func resourceClusterMaas() *schema.Resource {
 		ReadContext:   resourceClusterMaasRead,
 		UpdateContext: resourceClusterMaasUpdate,
 		DeleteContext: resourceClusterDelete,
-		Description:   "Resource for managing MAAS clusters in Spectro Cloud through Palette.",
+		Importer: &schema.ResourceImporter{
+			StateContext: resourceClusterMaasImport,
+		},
+		Description: "Resource for managing MAAS clusters in Spectro Cloud through Palette.",
 
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(60 * time.Minute),
@@ -33,9 +36,10 @@ func resourceClusterMaas() *schema.Resource {
 		SchemaVersion: 2,
 		Schema: map[string]*schema.Schema{
 			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "The name of the cluster.",
 			},
 			"context": {
 				Type:         schema.TypeString,
@@ -70,14 +74,15 @@ func resourceClusterMaas() *schema.Resource {
 					"Default value is `DownloadAndInstall`.",
 			},
 			"cloud_account_id": {
-				Type:     schema.TypeString,
-				Optional: true,
-				ForceNew: true,
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+				Description: "ID of the Maas cloud account used for the cluster. This cloud account must be of type `maas`.",
 			},
 			"cloud_config_id": {
 				Type:        schema.TypeString,
 				Computed:    true,
-				Description: "ID of the cloud config used for the cluster. This cloud config must be of type `azure`.",
+				Description: "ID of the cloud config used for the cluster. This cloud config must be of type `maas`.",
 				Deprecated:  "This field is deprecated and will be removed in the future. Use `cloud_config` instead.",
 			},
 			"os_patch_on_boot": {
@@ -116,8 +121,9 @@ func resourceClusterMaas() *schema.Resource {
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"domain": {
-							Type:     schema.TypeString,
-							Required: true,
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "Domain name in which the cluster to be provisioned.",
 						},
 					},
 				},
@@ -134,6 +140,7 @@ func resourceClusterMaas() *schema.Resource {
 							Elem: &schema.Schema{
 								Type: schema.TypeString,
 							},
+							Description: "Additional labels to be applied to the machine pool. Labels must be in the form of `key:value`.",
 						},
 						"node":   schemas.NodeSchema(),
 						"taints": schemas.ClusterTaintsSchema(),
@@ -155,6 +162,7 @@ func resourceClusterMaas() *schema.Resource {
 							Type:     schema.TypeString,
 							Required: true,
 							//ForceNew: true,
+							Description: "Name of the machine pool.",
 						},
 						"count": {
 							Type:        schema.TypeInt,
@@ -184,12 +192,14 @@ func resourceClusterMaas() *schema.Resource {
 							Elem: &schema.Resource{
 								Schema: map[string]*schema.Schema{
 									"min_memory_mb": {
-										Type:     schema.TypeInt,
-										Required: true,
+										Type:        schema.TypeInt,
+										Required:    true,
+										Description: "Minimum memory in MB required for the machine pool node.",
 									},
 									"min_cpu": {
-										Type:     schema.TypeInt,
-										Required: true,
+										Type:        schema.TypeInt,
+										Required:    true,
+										Description: "Minimum number of CPU required for the machine pool node.",
 									},
 								},
 							},
@@ -209,6 +219,7 @@ func resourceClusterMaas() *schema.Resource {
 							Elem: &schema.Schema{
 								Type: schema.TypeString,
 							},
+							Description: "Availability zones in which the machine pool nodes to be provisioned.",
 						},
 						"placement": {
 							Type:     schema.TypeList,
@@ -216,12 +227,14 @@ func resourceClusterMaas() *schema.Resource {
 							Elem: &schema.Resource{
 								Schema: map[string]*schema.Schema{
 									"id": {
-										Type:     schema.TypeString,
-										Computed: true,
+										Type:        schema.TypeString,
+										Computed:    true,
+										Description: "This is a computed(read-only) ID of the placement that is used to connect to the Maas cloud.",
 									},
 									"resource_pool": {
-										Type:     schema.TypeString,
-										Required: true,
+										Type:        schema.TypeString,
+										Required:    true,
+										Description: "The name of the resource pool in the Maas cloud.",
 									},
 								},
 							},
@@ -285,7 +298,6 @@ func resourceClusterMaasCreate(ctx context.Context, d *schema.ResourceData, m in
 	return diags
 }
 
-//goland:noinspection GoUnhandledErrorResult
 func resourceClusterMaasRead(_ context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	c := m.(*client.V1Client)
 
@@ -314,10 +326,22 @@ func flattenCloudConfigMaas(configUID string, d *schema.ResourceData, c *client.
 	if err != nil {
 		return diag.FromErr(err)
 	}
+	if err := ReadCommonAttributes(d); err != nil {
+		return diag.FromErr(err)
+	}
+
 	if config, err := c.GetCloudConfigMaas(configUID, ClusterContext); err != nil {
 		return diag.FromErr(err)
 	} else {
-		mp := flattenMachinePoolConfigsMaas(config.Spec.MachinePoolConfig)
+		if config.Spec != nil && config.Spec.CloudAccountRef != nil {
+			if err := d.Set("cloud_account_id", config.Spec.CloudAccountRef.UID); err != nil {
+				return diag.FromErr(err)
+			}
+		}
+		if err := d.Set("cloud_config", flattenClusterConfigsMaas(config)); err != nil {
+			return diag.FromErr(err)
+		}
+		mp := flattenMachinePoolConfigsMaas(config.Spec.MachinePoolConfig, config.Spec.ClusterConfig)
 		mp, err := flattenNodeMaintenanceStatus(c, d, c.GetNodeStatusMapMaas, mp, configUID, ClusterContext)
 		if err != nil {
 			return diag.FromErr(err)
@@ -330,7 +354,21 @@ func flattenCloudConfigMaas(configUID string, d *schema.ResourceData, c *client.
 	return diag.Diagnostics{}
 }
 
-func flattenMachinePoolConfigsMaas(machinePools []*models.V1MaasMachinePoolConfig) []interface{} {
+func flattenClusterConfigsMaas(config *models.V1MaasCloudConfig) []interface{} {
+	if config == nil || config.Spec == nil || config.Spec.ClusterConfig == nil {
+		return make([]interface{}, 0)
+	}
+
+	m := make(map[string]interface{})
+
+	if config.Spec.ClusterConfig.Domain != nil {
+		m["domain"] = *config.Spec.ClusterConfig.Domain
+	}
+
+	return []interface{}{m}
+}
+
+func flattenMachinePoolConfigsMaas(machinePools []*models.V1MaasMachinePoolConfig, config *models.V1MaasClusterConfig) []interface{} {
 
 	if machinePools == nil {
 		return make([]interface{}, 0)
@@ -361,6 +399,13 @@ func flattenMachinePoolConfigsMaas(machinePools []*models.V1MaasMachinePoolConfi
 		}
 
 		oi["azs"] = machinePool.Azs
+		if config != nil {
+			placement := make(map[string]interface{})
+			if config.Domain != nil {
+				placement["resource_pool"] = *config.Domain
+				oi["placement"] = []interface{}{placement}
+			}
+		}
 		//oi["resource_pool"] = machinePool.ResourcePool
 
 		ois[i] = oi
