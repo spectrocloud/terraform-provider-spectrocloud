64d612ceee9481b879acf81a8fed06359120afda
Sivaanand Murugesan <sivaanand.m@gmail.com>
2024-01-19 14:18:54 +0530

    reverting example changes

--- examples/resources/spectrocloud_macro/resource.tf
+++ examples/resources/spectrocloud_macro/resource.tf
@@ -1,10 +1,10 @@
 resource "spectrocloud_macro" "project_macro" {
   name    = "project1"
-  value   = "project_val2_updated"
+  value   = "project_val2"
   project = "Default"
 }
 
 resource "spectrocloud_macro" "tenant_macro" {
   name  = "tenant1"
-  value = "tenant_val1_updated"
+  value = "tenant_val1"
 }
