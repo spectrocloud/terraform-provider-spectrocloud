bbab6de7bad3e98b71a0f8a77eecf640055cba75
Sivaanand Murugesan <sivaanand.m@gmail.com>
2024-02-09 21:28:20 +0530

    updated examples

--- examples/resources/spectrocloud_custom_cloud_account/resource.tf
+++ examples/resources/spectrocloud_custom_cloud_account/resource.tf
@@ -1,5 +1,5 @@
 data "spectrocloud_private_cloud_gateway" "nutanix_pcg" {
-  name = "sh-nut-feb5a"
+  name = "test-pcg"
 }
 
 resource "spectrocloud_custom_cloud_account" "cloud_account" {
@@ -8,10 +8,10 @@ resource "spectrocloud_custom_cloud_account" "cloud_account" {
   private_cloud_gateway_id = data.spectrocloud_private_cloud_gateway.nutanix_pcg.id
   context = "tenant"
   credentials = {
-    "NUTANIX_USER" = "arvind1",
-    "NUTANIX_PASSWORD" = sensitive("Pni1NN985QF$"),
-    "NUTANIX_ENDPOINT" = "10.11.136.220",
-    "NUTANIX_PORT" = "9443",
+    "NUTANIX_USER" = "test_user",
+    "NUTANIX_PASSWORD" = sensitive("test123"),
+    "NUTANIX_ENDPOINT" = "10.12.11.22",
+    "NUTANIX_PORT" = "8998",
     "NUTANIX_INSECURE" = "yes"
   }
 }
