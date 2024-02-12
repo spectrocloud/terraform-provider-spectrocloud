6e4336b5a586da14d1eeeb56595264d287b99c36
Sivaanand Murugesan <sivaanand.m@gmail.com>
2024-02-12 10:47:30 +0530

    PLT-987:Add support for profile variables. (#418)
    * PLT-987:Add support for profile variables.
    
    * updated go mod
    
    * Restricted the profile varaibles to edge-native cloud type profiles.
    
    * updated unit test

---
+++ spectrocloud/resource_cluster_profile_test.go
@@ -0,0 +1,161 @@
+package spectrocloud
+
+import (
+	"github.com/spectrocloud/gomi/pkg/ptr"
+	"github.com/spectrocloud/hapi/models"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestToClusterProfileVariables(t *testing.T) {
+
+	mockResourceData := resourceClusterProfile().TestResourceData()
+	var proVar []interface{}
+	variables := map[string]interface{}{
+		"variable": []interface{}{
+			map[string]interface{}{
+				"default_value": "default_value_1",
+				"description":   "description_1",
+				"display_name":  "display_name_1",
+				"format":        "string",
+				"hidden":        false,
+				"immutable":     true,
+				"name":          "variable_name_1",
+				"regex":         "regex_1",
+				"required":      true,
+				"is_sensitive":  false,
+			},
+			map[string]interface{}{
+				"default_value": "default_value_2",
+				"description":   "description_2",
+				"display_name":  "display_name_2",
+				"format":        "integer",
+				"hidden":        true,
+				"immutable":     false,
+				"name":          "variable_name_2",
+				"regex":         "regex_2",
+				"required":      false,
+				"is_sensitive":  true,
+			},
+		},
+	}
+	proVar = append(proVar, variables)
+	mockResourceData.Set("cloud", "edge-native")
+	mockResourceData.Set("profile_variables", proVar)
+	result, err := toClusterProfileVariables(mockResourceData)
+
+	// Assertions for valid profile variables
+	assert.NoError(t, err)
+	assert.Len(t, result, 2)
+
+	// Test case 2: Empty profile variables
+	mockResourceDataEmpty := resourceClusterProfile().TestResourceData()
+	mockResourceDataEmpty.Set("cloud", "edge-native")
+	mockResourceDataEmpty.Set("profile_variables", []interface{}{map[string]interface{}{}})
+	resultEmpty, errEmpty := toClusterProfileVariables(mockResourceDataEmpty)
+
+	// Assertions for empty profile variables
+	assert.NoError(t, errEmpty)
+	assert.Len(t, resultEmpty, 0)
+
+	// Test case 3: Invalid profile variables format
+	mockResourceDataInvalid := resourceClusterProfile().TestResourceData()
+	mockResourceDataInvalid.Set("cloud", "edge-native")
+	mockResourceDataInvalid.Set("profile_variables", []interface{}{
+		map[string]interface{}{
+			"variable": []interface{}{}, // Invalid format, should be a list
+		},
+	})
+	resultInvalid, _ := toClusterProfileVariables(mockResourceDataInvalid)
+
+	// Assertions for invalid profile variables format
+	assert.Len(t, resultInvalid, 0) // No variables should be extracted on error
+}
+
+func TestFlattenProfileVariables(t *testing.T) {
+	// Test case 1: Valid profile variables and pv
+	mockResourceData := resourceClusterProfile().TestResourceData()
+	var proVar []interface{}
+	variables := map[string]interface{}{
+		"variable": []interface{}{
+			map[string]interface{}{
+				"name":          "variable_name_1",
+				"display_name":  "display_name_1",
+				"description":   "description_1",
+				"format":        "string",
+				"default_value": "default_value_1",
+				"regex":         "regex_1",
+				"required":      true,
+				"immutable":     false,
+				"hidden":        false,
+			},
+			map[string]interface{}{
+				"name":          "variable_name_2",
+				"display_name":  "display_name_2",
+				"description":   "description_2",
+				"format":        "integer",
+				"default_value": "default_value_2",
+				"regex":         "regex_2",
+				"required":      false,
+				"immutable":     true,
+				"hidden":        true,
+			},
+		},
+	}
+	proVar = append(proVar, variables)
+	mockResourceData.Set("cloud", "edge-native")
+	mockResourceData.Set("profile_variables", proVar)
+
+	pv := []*models.V1Variable{
+		{Name: ptr.StringPtr("variable_name_1"), DisplayName: "display_name_1", Description: "description_1", Format: "string", DefaultValue: "default_value_1", Regex: "regex_1", Required: true, Immutable: false, Hidden: false},
+		{Name: ptr.StringPtr("variable_name_2"), DisplayName: "display_name_2", Description: "description_2", Format: "integer", DefaultValue: "default_value_2", Regex: "regex_2", Required: false, Immutable: true, Hidden: true},
+	}
+
+	result, err := flattenProfileVariables(mockResourceData, pv)
+
+	// Assertions for valid profile variables and pv
+	assert.NoError(t, err)
+	assert.Len(t, result, 1)
+	assert.Equal(t, []interface{}{
+		map[string]interface{}{
+			"variable": []interface{}{
+				map[string]interface{}{
+					"name":          ptr.StringPtr("variable_name_1"),
+					"display_name":  "display_name_1",
+					"description":   "description_1",
+					"format":        models.V1VariableFormat("string"),
+					"default_value": "default_value_1",
+					"regex":         "regex_1",
+					"required":      true,
+					"immutable":     false,
+					"hidden":        false,
+					"is_sensitive":  false,
+				},
+				map[string]interface{}{
+					"name":          ptr.StringPtr("variable_name_2"),
+					"display_name":  "display_name_2",
+					"description":   "description_2",
+					"format":        models.V1VariableFormat("integer"),
+					"default_value": "default_value_2",
+					"regex":         "regex_2",
+					"required":      false,
+					"immutable":     true,
+					"hidden":        true,
+					"is_sensitive":  false,
+				},
+			},
+		},
+	}, result)
+
+	// Test case 2: Empty profile variables and pv
+	//mockResourceDataEmpty := schema.TestResourceDataRaw(t, resourceClusterProfileVariables().Schema, map[string]interface{}{})
+	mockResourceDataEmpty := resourceClusterProfile().TestResourceData()
+	mockResourceDataEmpty.Set("cloud", "edge-native")
+	mockResourceDataEmpty.Set("profile_variables", []interface{}{map[string]interface{}{}})
+	resultEmpty, errEmpty := flattenProfileVariables(mockResourceDataEmpty, nil)
+
+	// Assertions for empty profile variables and pv
+	assert.NoError(t, errEmpty)
+	assert.Len(t, resultEmpty, 0)
+	assert.Equal(t, []interface{}{}, resultEmpty)
+}
