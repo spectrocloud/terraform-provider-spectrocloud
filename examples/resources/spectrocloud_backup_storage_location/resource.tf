4a7d18b3b497066f45cc3da004ede67c8577679a
Sivaanand Murugesan <sivaanand.m@gmail.com>
2024-10-17 14:05:34 +0530

    fix reviewble

--- examples/resources/spectrocloud_backup_storage_location/resource.tf
+++ examples/resources/spectrocloud_backup_storage_location/resource.tf
@@ -1,6 +1,6 @@
 resource "spectrocloud_backup_storage_location" "bsl1" {
   name        = "aaa-project-dev-1"
-  context = "project"
+  context     = "project"
   is_default  = false
   region      = "us-east-1"
   bucket_name = "project-backup-2"
@@ -9,7 +9,7 @@ resource "spectrocloud_backup_storage_location" "bsl1" {
     access_key          = "access_key"
     secret_key          = "secret_key"
     s3_force_path_style = false
-    s3_url             = "http://10.90.78.23"
+    s3_url              = "http://10.90.78.23"
   }
 }
 
