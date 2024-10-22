70ba2f374fab9bf7b4181e48483dadab76af5c7c
Sivaanand Murugesan <sivaanand.m@gmail.com>
2024-10-22 18:29:23 +0530

    PLT-1402:Added ssh_key management support into terrafrom

---
+++ spectrocloud/data_source_ssh_key.go
@@ -0,0 +1,73 @@
+package spectrocloud
+
+import (
+	"context"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/spectrocloud/palette-sdk-go/api/models"
+)
+
+func dataSourceSSHKey() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceSSHKeyRead,
+
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:          schema.TypeString,
+				Computed:      true,
+				Optional:      true,
+				ConflictsWith: []string{"name"},
+				Description:   "The Id of the SSH key resource.",
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Optional:    true,
+				Description: "The name of the SSH key resource.",
+			},
+			"ssh_key": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Sensitive:   true,
+				Description: "The SSH key value.",
+			},
+			"context": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "project",
+				ValidateFunc: validation.StringInSlice([]string{"project", "tenant"}, false),
+				Description: "The context of the cluster profile. Allowed values are `project` or `tenant`. " +
+					"Default value is `project`. " + PROJECT_NAME_NUANCE,
+			},
+		},
+	}
+}
+
+func dataSourceSSHKeyRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	sshKeyContext := d.Get("context").(string)
+	c := getV1ClientWithResourceContext(m, sshKeyContext)
+	var diags diag.Diagnostics
+	id := d.Get("id").(string)
+	name := d.Get("name").(string)
+	var sshKey *models.V1UserAssetSSH
+	var err error
+	if id != "" {
+		sshKey, err = c.GetSSHKey(d.Id())
+	} else if name != "" {
+		sshKey, err = c.GetSSHKeyByName(name)
+	}
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	d.SetId(sshKey.Metadata.UID)
+	err = d.Set("name", sshKey.Metadata.Name)
+	if err != nil {
+		return nil
+	}
+	err = d.Set("ssh_key", sshKey.Spec.PublicKey)
+	if err != nil {
+		return nil
+	}
+	return diags
+}
