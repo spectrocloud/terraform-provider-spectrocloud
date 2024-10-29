743cdb46dc010ef9fe5add3f4249e4c0a7a51f12
Sivaanand Murugesan <sivaanand.m@gmail.com>
2024-10-29 10:29:57 +0530

    example fix

--- examples/data-sources/spectrocloud_privatecloudgateway_dns_map/datasource.tf
+++ examples/data-sources/spectrocloud_privatecloudgateway_dns_map/datasource.tf
@@ -3,8 +3,9 @@ data "spectrocloud_private_cloud_gateway" "gateway" {
 }
 
 data "spectrocloud_privatecloudgateway_dns_map" "gateway_dns_map" {
-  search_domain_name = "spectrocloud2.dev"
-
+  search_domain_name = "spectrocloud.dev"
+  # Option to filter with network, if more than one dns map in same search_domain_name.
+  # network = "VM-NETWORK"
   private_cloud_gateway_id = data.spectrocloud_private_cloud_gateway.gateway.id
 }
 
