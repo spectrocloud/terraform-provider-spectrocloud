ebca374afb079cdb8886785d15ff4957c975f03a
Sivaanand Murugesan <sivaanand.m@gmail.com>
2024-12-25 21:30:46 +0530

    removed commented code

--- spectrocloud/resource_cluster_profile.go
+++ spectrocloud/resource_cluster_profile.go
@@ -477,16 +477,6 @@ func toClusterProfileVariables(d *schema.ResourceData) ([]*models.V1Variable, er
 	var profileVariables []*models.V1Variable
 	if pVariables, ok := d.GetOk("profile_variables"); ok {
 
-		// Once the profile_Variables feature is extended to all cloud types, the following block should be removed.
-		//cloudType, _ := d.Get("cloud").(string)
-		//profileType, _ := d.Get("type").(string)
-		//if cloudType != "edge-native" {
-		//	if profileType != "add-on" {
-		//		err := errors.New("currently, `profile_variables` is only supported for the `add-on` profile type and other profile type is supported only for edge-native cloud type")
-		//		return profileVariables, err
-		//	}
-		//}
-
 		if pVariables.([]interface{})[0] != nil {
 			variables := pVariables.([]interface{})[0].(map[string]interface{})["variable"]
 			for _, v := range variables.([]interface{}) {
