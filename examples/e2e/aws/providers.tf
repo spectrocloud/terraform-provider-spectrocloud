d4ec677157945fcc34615815f8978b8b213275d0
Attili Sandeep <attilisandeep@Attilis-MacBook-Pro.local>
2025-03-17 16:22:14 +0530

    adding changes

--- examples/e2e/aws/providers.tf
+++ examples/e2e/aws/providers.tf
@@ -1,10 +1,18 @@
 terraform {
   required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
     spectrocloud = {
       version = ">= 0.1"
       source  = "spectrocloud/spectrocloud"
     }
   }
+}
+
+provider "aws" {
+  region = var.aws_region
 }
 
 variable "sc_host" {
