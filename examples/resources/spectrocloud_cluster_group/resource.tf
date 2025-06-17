8abed7a5b7ca02a6a733200c785c8c844429a01d
Sivaanand Murugesan <sivaanand.m@gmail.com>
2025-06-17 13:21:37 +0530

    clusterprofile fix
    Signed-off-by: Sivaanand Murugesan <sivaanand.m@gmail.com>

---
+++ examples/resources/spectrocloud_cluster_group/resource.tf
@@ -0,0 +1,22 @@
+resource "spectrocloud_cluster_group" "cg" {
+  name        = "ran-cp-cluster-group"
+  context     = "tenant"
+  description = "Cluster Group description updated"
+  tags        = ["qa:dev"]
+  
+  config {
+    host_endpoint_type      = "Ingress"
+    cpu_millicore          = 12000
+    memory_in_mb          = 16384
+    storage_in_gb         = 12
+    oversubscription_percent = 120
+    values                = ""
+    k8s_distribution      = "k3s"
+  }
+
+  clusters {
+      cluster_uid = "684fba868619ff5e691f0741"
+      host_dns    = "*.dev.spectrocloud.com"
+    }
+
+}
