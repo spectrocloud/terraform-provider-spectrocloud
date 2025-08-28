9a3443595a18350ca89c78ed21b857c3477081ba
Sivaanand Murugesan <sivaanand.m@gmail.com>
2025-08-28 11:59:13 +0530

    Fixed alert import
    Signed-off-by: Sivaanand Murugesan <sivaanand.m@gmail.com>

--- spectrocloud/resource_alert_import.go
+++ spectrocloud/resource_alert_import.go
@@ -3,16 +3,30 @@ package spectrocloud
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func resourceAlertImport(ctx context.Context, d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, error) {
-	// Set a placeholder name for now - the read function will populate the correct data
-	if err := d.Set("name", "imported_alert"); err != nil {
+	c := getV1ClientWithResourceContext(m, "tenant")
+	idParts := strings.Split(d.Id(), ":")
+	if len(idParts) != 2 {
+		return nil, fmt.Errorf("invalid import ID format, expected 'alertUid:component', got: %s", d.Id())
+	}
+	alertUid := idParts[0]
+	component := idParts[1]
+	pjt, err := c.GetProject(ProviderInitProjectUid)
+	if err != nil {
 		return nil, err
 	}
-
+	if err := d.Set("project", pjt.Metadata.Name); err != nil {
+		return nil, err
+	}
+	if err := d.Set("component", component); err != nil {
+		return nil, err
+	}
+	d.SetId(alertUid)
 	// Read all alert data to populate the state
 	diags := resourceAlertRead(ctx, d, m)
 	if diags.HasError() {
