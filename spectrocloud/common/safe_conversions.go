314b2fa657efa62f0ff9ac3a35b2accb04ab2932
Sivaanand Murugesan <sivaanand.m@gmail.com>
2025-09-22 12:12:13 +0530

    go sec fix

--- spectrocloud/common/safe_conversions.go
+++ spectrocloud/common/safe_conversions.go
@@ -9,11 +9,17 @@ func SafeUint32(value int) uint32 {
 	// On 64-bit systems, we need to check against uint32 max
 	if ^uint(0)>>32 == 0 {
 		// 32-bit system: int and uint32 have same size, no overflow possible
-		return uint32(value)
+		if value >= 0 {
+			return uint32(value)
+		}
+		return 0
 	}
 	// 64-bit system: check against uint32 max
 	if uint64(value) > 0xFFFFFFFF {
 		return 0xFFFFFFFF
 	}
-	return uint32(value)
+	if value >= 0 {
+		return uint32(value)
+	}
+	return 0
 }
