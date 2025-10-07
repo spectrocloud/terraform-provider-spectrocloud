956a426f70532781cdf9a3033fd84303c610f67f
Sivaanand Murugesan <sivaanand.m@gmail.com>
2025-10-08 03:27:25 +0530

    go sec fix

--- spectrocloud/kubevirt/schema/virtualmachineinstance/domain_spec.go
+++ spectrocloud/kubevirt/schema/virtualmachineinstance/domain_spec.go
@@ -533,7 +533,10 @@ func flattenDisks(in []kubevirtapiv1.Disk) []interface{} {
 		c["disk_device"] = flattenDiskDevice(v.DiskDevice)
 		c["serial"] = v.Serial
 		if v.BootOrder != nil {
-			c["boot_order"] = int(*v.BootOrder)
+			// Safe conversion from uint to int to avoid potential overflow
+			if *v.BootOrder <= math.MaxInt32 {
+				c["boot_order"] = int(*v.BootOrder)
+			}
 		}
 
 		att[i] = c
