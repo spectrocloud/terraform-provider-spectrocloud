6600710b75ef889a294d97c15721138b178b9629
Sivaanand Murugesan <sivaanand.m@gmail.com>
2025-12-02 15:38:57 +0530

    PLT-2026: Fixed add-on deployment resource taint issue. (#725)
    * PLT-2026: Fixed addon deployment resource taint issue.
    
    * reviewable fix
    
    * fixed addondeployment

---
+++ examples/resources/spectrocloud_addon_deployment/resource.tf
@@ -0,0 +1,21 @@
+# Example 1: Basic addon deployment with a single pack from public registry
+resource "spectrocloud_addon_deployment" "example_basic" {
+  cluster_uid = var.cluster_uid
+
+  cluster_profile {
+    id = var.cluster_profile_uid
+    variables = {
+      test = "IfNotPresent"
+    }
+    pack {
+      name   = "nginx-ingress"
+      tag    = "4.7.1"
+      uid    = data.spectrocloud_pack.nginx.id
+      values = <<-EOT
+        controller:
+          service:
+            type: LoadBalancer
+      EOT
+    }
+  }
+}
