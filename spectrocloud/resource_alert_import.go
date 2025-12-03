03045a7249eee24304641e8df747e57ae84f87b2
Manimaran <manimaran89@gmail.com>
2025-12-03 12:26:15 +0530

    PLT-2058 Migrate to use TypeSet for Resource TF Provider spectrocloud_alert (#728)
    * Migrate to use TypeSet for Resource TF Provider spectrocloud_alert
    
    * Converting list to set for resource alert
    
    * Revert "Converting list to set for resource alert"
    
    Reverting back to Original changes from typeList to typeSet
    This reverts commit 581e060374335582eea1f369ae49c626fd33a89f.
    
    * PLT-2048 Migrate to use TypeSet for Resource TF Provider spectrocloud_alert
    
    * PLT-2048 Migrate to use TypeSet for Resource TF Provider spectrocloud_alert
    
    * PLT-2048 Migrate to use TypeSet for Resource TF Provider spectrocloud_alert
    
    * PLT-2048 Migrate to use TypeSet for Resource TF Provider spectrocloud_alert
    
    * PLT-2058 Migrate to use TypeSet for Resource TF Provider spectrocloud_alert
    
    * PLT-2058 Migrate to use TypeSet for Resource TF Provider spectrocloud_alert
    
    * PLT-2058 Migrate to use TypeSet for Resource TF Provider spectrocloud_alert
    
    * PLT-2058 Migrate to use TypeSet for Resource TF Provider spectrocloud_alert
    
    * PLT-2058 Migrate to use TypeSet for Resource TF Provider spectrocloud_alert
    
    * PLT-2058 Migrate to use TypeSet for Resource TF Provider spectrocloud_alert
    
    * PLT-2058: Fixed alert functionality as par with palette UI
    
    * fix: updated go version for govul check.
    
    ---------
    
    Co-authored-by: Sivaanand Murugesan <sivaanand.m@gmail.com>

--- spectrocloud/resource_alert_import.go
+++ spectrocloud/resource_alert_import.go
@@ -3,26 +3,63 @@ package spectrocloud
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func resourceAlertImport(ctx context.Context, d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, error) {
+	// Expected ID format: projectUID:component OR projectName:component
+	idParts := strings.Split(d.Id(), ":")
 
-	idParts := d.Id()
-	context := "project"
-	c := getV1ClientWithResourceContext(m, context)
-	pjt, err := c.GetProject(ProviderInitProjectUid)
+	if len(idParts) != 2 {
+		return nil, fmt.Errorf("invalid import ID format. Expected: 'projectUID:component' or 'projectName:component', got: %s", d.Id())
+	}
+
+	projectIdentifier := idParts[0]
+	component := idParts[1]
+
+	// Validate component
+	if component != "ClusterHealth" {
+		return nil, fmt.Errorf("invalid component: %s. Only 'ClusterHealth' is supported", component)
+	}
+
+	c := getV1ClientWithResourceContext(m, "")
+
+	// Try to get project by UID first, then by name
+	var projectName string
+	var projectUID string
+
+	// Check if projectIdentifier is a UID or name by trying to get the project
+	pjt, err := c.GetProject(projectIdentifier)
 	if err != nil {
-		return nil, err
+		// If failed, try to get project UID from name
+		projectUID, err = c.GetProjectUID(projectIdentifier)
+		if err != nil {
+			return nil, fmt.Errorf("could not find project with identifier '%s': %v", projectIdentifier, err)
+		}
+		// Get project details using the UID
+		pjt, err = c.GetProject(projectUID)
+		if err != nil {
+			return nil, fmt.Errorf("could not get project details for UID '%s': %v", projectUID, err)
+		}
+		projectName = pjt.Metadata.Name
+	} else {
+		projectName = pjt.Metadata.Name
+		projectUID = pjt.Metadata.UID
 	}
-	if err := d.Set("project", pjt.Metadata.Name); err != nil {
-		return nil, err
+
+	// Set the project and component in state
+	if err := d.Set("project", projectName); err != nil {
+		return nil, fmt.Errorf("error setting project: %v", err)
 	}
-	if err := d.Set("component", "ClusterHealth"); err != nil {
-		return nil, err
+	if err := d.Set("component", component); err != nil {
+		return nil, fmt.Errorf("error setting component: %v", err)
 	}
-	d.SetId(idParts)
+
+	// Set the canonical ID format
+	d.SetId(fmt.Sprintf("%s:%s", projectUID, component))
+
 	// Read all alert data to populate the state
 	diags := resourceAlertRead(ctx, d, m)
 	if diags.HasError() {
