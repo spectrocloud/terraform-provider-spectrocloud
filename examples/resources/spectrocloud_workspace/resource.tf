46c678bd48468903cb70601ae0d49f459d79d60a
Manimaran <manimaran.avudayappan@spectrocloud.com>
2026-01-19 17:12:07 +0530

    Made changes in documentation for gpu

--- examples/resources/spectrocloud_workspace/resource.tf
+++ examples/resources/spectrocloud_workspace/resource.tf
@@ -44,7 +44,7 @@ resource "spectrocloud_workspace" "workspace" {
     resource_allocation = {
       cpu_cores    = "8"
       memory_MiB   = "8192"
-      gpu_limit    = "2"
+      gpu          = "2"
       gpu_provider = "nvidia"
     }
 
