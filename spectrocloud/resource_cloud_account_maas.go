051a3c3b43b24497a581e38c4544050cf2c53b71
Manimaran <manimaran.avudayappan@spectrocloud.com>
2026-02-13 16:36:06 +0530

    Setting the API endpoint in read function

--- spectrocloud/resource_cloud_account_maas.go
+++ spectrocloud/resource_cloud_account_maas.go
@@ -96,6 +96,10 @@ func resourceCloudAccountMaasRead(_ context.Context, d *schema.ResourceData, m i
 		return diag.FromErr(err)
 	}
 
+	if err := d.Set("maas_api_endpoint", *account.Spec.APIEndpoint); err != nil {
+		return diag.FromErr(err)
+	}
+
 	return diags
 }
 
