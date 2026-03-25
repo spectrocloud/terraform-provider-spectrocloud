3d6781dfc0c1d4b504963058a9d745d89722a896
Manimaran <manimaran.avudayappan@spectrocloud.com>
2026-03-25 16:42:11 +0530

    PLT-2186 Fixing the virtual_machine: inline data_volume_templates fails during creation (#857)
    * Fixing the data_volume issue while creation
    
    * Adding the go mod
    
    * Adding the go mod
    
    * Adding the fix for govulncheck
    
    * Adding the fix for int

--- spectrocloud/kubevirt/schema/virtualmachineinstance/domain_spec.go
+++ spectrocloud/kubevirt/schema/virtualmachineinstance/domain_spec.go
@@ -2,6 +2,7 @@ package virtualmachineinstance
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
@@ -112,7 +113,11 @@ func expandDisksToVM(disks []interface{}) []*models.V1VMDisk {
 			disk.Serial = v
 		}
 		if v, ok := in["boot_order"].(int); ok && v > 0 {
-			disk.BootOrder = int32(v)
+			if uint64(v) > uint64(math.MaxInt32) {
+				disk.BootOrder = math.MaxInt32
+			} else {
+				disk.BootOrder = int32(v) // #nosec G115 -- v > 0 and bounded by math.MaxInt32 above
+			}
 		}
 		if v, ok := in["disk_device"].([]interface{}); ok && len(v) > 0 {
 			expandDiskDeviceToVM(v, disk)
